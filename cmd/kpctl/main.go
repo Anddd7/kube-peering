0142881a48aa953c4b7a583d15f72e2bb67688a0
Anddd7 <liaoad_space@sina.com>
2023-03-16 21:50:26 +0800

    feat: init cli skeleton with cobra

---
+++ cmd/kpctl/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Anddd7 <liaoad_space@sina.com>
+*/
+package main
+
+import "github.com/kube-peering/cmd/kpctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ cmd/kpeering/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Anddd7 <liaoad_space@sina.com>
+*/
+package main
+
+import "github.com/kube-peering/cmd/kpeering/cmd"
+
+func main() {
+	cmd.Execute()
+}
