2099f0c0033dc44292b03b6e1ac567448a73ac89
Anddd7 <liaoad_space@sina.com>
2023-03-16 21:50:58 +0800

    infra: add makefile to build the cmd

---
+++ Makefile
@@ -0,0 +1,21 @@
+clean:
+	rm -rf bin
+
+fmt:
+	go fmt ./...
+	gofumpt -l -w .
+	go vet ./...
+
+lint:
+	golangci-lint run -v
+
+cover:
+	go test ./... -coverprofile ./bin/coverage.out
+
+coverweb: cover
+	go tool cover -html=./bin/coverage.out
+
+check: fmt lint cover
+
+dep:
+	go mod download
---
+++ scripts/cmd.mk
@@ -0,0 +1,23 @@
+# required parameters
+# COMMAND
+
+ROOT_PATH 		= $(shell git rev-parse --show-toplevel 2>/dev/null)
+BUILD_BINARY 	= $(ROOT_PATH)/bin/$(COMMAND)
+
+run:
+	go run ./
+
+build:
+	go build -o $(BUILD_BINARY) ./
+
+test:
+	go test ./...
+
+clean:
+	rm $(BUILD_BINARY)
+
+install: build
+	mv $(BUILD_BINARY) /usr/local/bin/$(COMMAND) 
+
+uninstall:
+	rm /usr/local/bin/$(COMMAND)
