d4c96532da8ae10427f7bade75470147dbc339c2
Anddd7 <liaoad_space@sina.com>
2023-04-02 23:10:21 +0800

    feat: example for http reverse proxy

--- example/config.go
+++ example/config.go
@@ -2,8 +2,8 @@ package http2
 
 import "github.com/kube-peering/internal/pkg/config"
 
-var appTlsConfig, _ = config.LoadServerTlsConfig("../../certs_app/server.crt", "../../certs_app/server.key", "localhost")
-var clientTlsConfig, _ = config.LoadClientTlsConfig("../../certs_app/ca.crt", "localhost")
+var AppTlsConfig, _ = config.LoadServerTlsConfig("../../certs_app/server.crt", "../../certs_app/server.key", "localhost")
+var ClientTlsConfig, _ = config.LoadClientTlsConfig("../../certs_app/ca.crt", "localhost")
 
-var tunnelServerTlsConfig, _ = config.LoadServerTlsConfig("../../certs_tunnel/server.crt", "../../certs_app/server.key", "localhost")
-var tunnelClientTlsConfig, _ = config.LoadClientTlsConfig("../../certs_tunnel/ca.crt", "localhost")
+var TunnelServerTlsConfig, _ = config.LoadServerTlsConfig("../../certs_tunnel/server.crt", "../../certs_app/server.key", "localhost")
+var TunnelClientTlsConfig, _ = config.LoadClientTlsConfig("../../certs_tunnel/ca.crt", "localhost")
--- example/http/transit/main.go
+++ example/http/transit/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"net"
+	"os"
 
 	"github.com/kube-peering/internal/pkg/transit"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "http" {
+		http()
+	} else {
+		tcp()
+	}
+}
+
+func tcp() {
 	interceptor := transit.NewInterceptor("tcp", 10021)
 	forwarder := transit.NewFowarder("tcp", ":8080")
-	interceptor.OnTCPConnected = func(conn *net.TCPConn) {
-		forwarder.ForwardTCP(conn)
-	}
+	interceptor.OnTCPConnected = forwarder.ForwardTCP
 
+	interceptor.Start()
+}
+
+func http() {
+	interceptor := transit.NewInterceptor("http", 10021)
+	forwarder := transit.NewFowarder("http", "http://localhost:8080")
+	interceptor.OnHTTPConnected = forwarder.ForwardHttp
 	interceptor.Start()
 }
